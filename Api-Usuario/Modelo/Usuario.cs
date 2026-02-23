ebdb667d2c9e949539251e6ca57934b19e1a2861
LuisMhp <119384750+Luis-Miguel-HP@users.noreply.github.com>
2026-02-23 11:14:36 -0400

    Primer Commit

--- Api-Usuario/Modelo/Usuario.cs
+++ Api-Usuario/Modelo/Usuario.cs
@@ -1,10 +1,22 @@
-﻿namespace Api_Usuario.Modelo
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_Usuario.Modelo
 {
     public class Usuario
     {
+
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
         public int Id { get; set; }
+
+        [Required]
         public string Nombre { get; set; }
+
+        [Required]
         public string Correo { get; set; }
+
+        [Required]
         public DateTime FechaDeNacimiento { get; set; }
 
     }
