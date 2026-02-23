396d3c2eefe764b17e17275dbdab6603cc83e05f
LuisMhp <119384750+Luis-Miguel-HP@users.noreply.github.com>
2026-02-23 12:55:51 -0400

    Creamos el contexto

---
+++ Api-Usuario/Context/UsuarioContext.cs
@@ -0,0 +1,18 @@
+﻿using Api_Usuario.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+
+// este achivo es el que tiene la esencia de entity framework
+namespace Api_Usuario.Context
+{
+    public class UsuarioContext : DbContext
+    {
+        public UsuarioContext(DbContextOptions<UsuarioContext> options)
+              : base(options)
+        {
+          
+            
+        }
+        public DbSet<Usuario> Usuarios { get; set; }
+    }
+}
